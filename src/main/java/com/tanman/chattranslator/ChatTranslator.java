ac762e7cc5666d2ef737d026077712128a73f5b6
Tanmay <tanmay.master138@gmail.com>
2026-07-29 07:18:20 +0400

    chore: scaffold Fabric mod project

---
+++ src/main/java/com/tanman/chattranslator/ChatTranslator.java
@@ -0,0 +1,16 @@
+package com.tanman.chattranslator;
+
+import net.fabricmc.api.ModInitializer;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class ChatTranslator implements ModInitializer {
+	public static final String MOD_ID = "chat-translator";
+
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	@Override
+	public void onInitialize() {
+	}
+}
